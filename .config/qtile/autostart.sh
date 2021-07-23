2740f470f959a2aff0d04b80a09df99a1cf1762e
Brown Bishop <catalin1second@gmail.com>
2021-07-23 09:15:02 +0300

    minor changes

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 picom --experimental-backends &
 nitrogen --restore &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & 
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
