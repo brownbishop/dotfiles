c2f02b6a7c5e413812b689417e80989ffb3eb909
brownbishop <catalin1second@gmail.com>
2021-11-17 09:47:13 +0200

    change default browser and some cleanups

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,26 +1,17 @@
 import os
-import re
-import psutil
 import socket
 import subprocess
 
 from libqtile.config import Key, Screen, Group, Drag, Click, Match
 from libqtile.lazy import lazy
-from libqtile import qtile, layout, bar, widget, hook
+from libqtile import layout, bar, widget, hook
 
-from typing import List  # noqa: F401
+from typing import List
 
 # set mod key to mod(also known as the Windows key)
 mod = "mod4"
 myTerm = "xterm"
-
-# set terminal emulator
-if qtile.core.name == "x11":
-    myTerm = "xterm"
-    dmenu_command = "dmenu_run"
-elif qtile.core.name == "wayland":
-    myTerm = "gnome-terminal"
-    dmenu_command = "cage dmenu_run"
+dmenu_command = "dmenu_run"
 
 keys = [
     # Launch terminal emulator
@@ -45,7 +36,7 @@ keys = [
     Key([mod], "b", lazy.hide_show_bar("top")),
 
     # Web browser shortcut
-    Key([mod], "w", lazy.spawn("brave")),
+    Key([mod], "w", lazy.spawn("chromium")),
 
     # Toggle screensaver
     Key([mod], "s", lazy.spawn("i3lock -i ./wall.png")),
@@ -115,6 +106,7 @@ keys = [
         lazy.layout.toggle_split(),
         desc='Toggle between split and unsplit sides of stack'
         ),
+
     #Key([mod], ",", lazy.to_screen(0)),
     #Key([mod], ".", lazy.to_screen(1)),
     # Volume controls
@@ -366,12 +358,12 @@ screens = [
             24,
         ),
     ),
-    Screen(
-        top=bar.Bar(
-            create_widgets(),
-            24,
-        ),
-    ),
+#    Screen(
+#        top=bar.Bar(
+#            create_widgets(),
+#            24,
+#        ),
+#    ),
 ]
 
 # Drag floating layouts.
@@ -403,60 +395,14 @@ floating_layout = layout.Floating(float_rules=[
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
-# hooks
 
+# hooks
 
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser('~')
     subprocess.call([home + '/.config/qtile/autostart.sh'])
 
-
-# # Window swallowing
-# # I don't use it right now
-#
-# # works only for programs listed here
-# # if you want to add or remove programs
-# # modify the "terminals" list
-# def is_terminal(window_cmd):
-#     # list of programs that will be hidden when a
-#     # child process (that opens a window) is spawned
-#     # it's called terminals because I only use it
-#     # for terminal emulators
-#     terminals = ["alacritty", "st", "gnome-terminal"]
-#     return window_cmd in terminals
-#
-# @hook.subscribe.client_new
-# def _swallow(window):
-#     # pid of current window
-#     pid = window.window.get_net_wm_pid()
-#
-#     # parents of the process running in the current window
-#     parents = psutil.Process(pid).parents()
-#     if parents == []:
-#         return
-#
-#     # dictionary {pid: window} to access all windows by pid
-#     cpids = {c.window.get_net_wm_pid(): wid for wid,
-#              c in window.qtile.windows_map.items()}
-#
-#     # find the first parent that has a window opened then minimize it
-#     # parents are sorted in reverse pid order, feel free to check out
-#     for i in parents:
-#         if i.pid in cpids and is_terminal(i.name()):
-#             # find parent and minimize it
-#             parent = window.qtile.windows_map.get(cpids[i.pid])
-#             if not parent.minimized: # make sure it wasn't swallowed by another process
-#                 parent.minimized = True
-#                 # set parent attribute to current window
-#                 window.parent = parent
-#                 return
-#
-# @hook.subscribe.client_killed
-# def _unswallow(window):
-#     if hasattr(window, 'parent'):
-#         window.parent.minimized = False
-#
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
