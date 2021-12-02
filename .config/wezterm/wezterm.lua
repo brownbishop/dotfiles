9b00881cd6fb864bb8ba5068ccd1b7ba920dce79
brownbishop <catalin1second@gmail.com>
2021-12-02 19:03:13 +0200

    fonts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("JetBrains Mono"),
+  font = wezterm.font("Iosevka Term"),
   color_scheme = "Gruvbox Dark",
   window_background_opacity = 0.9,
   enable_tab_bar = false,
