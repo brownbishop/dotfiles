d5c5bf917b9538028b083232c1c3fbdd800e42d3
brownbishop <catalin1second@gmail.com>
2022-06-05 17:31:52 +0300

    more idiomatic

--- .config/nvim/lua/cmp-config.lua
+++ .config/nvim/lua/cmp-config.lua
@@ -50,7 +50,7 @@ cmp.setup {
         ['<CR>'] = cmp.mapping.confirm({
             select = true,
         }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ["<C-n>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -62,7 +62,7 @@ cmp.setup {
           end
         end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        ["<C-p>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
