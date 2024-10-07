161b5d0678de88da06e94700993d54fe5b79e0ed
git <catalin1second@gmail.com>
2024-10-07 20:04:33 +0300

    new begining

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,17 @@
-local wezterm = require 'wezterm';
-return {
-  font = wezterm.font("JetBrains Mono"),
-  color_scheme = "Gruvbox Dark",
-  window_background_opacity = 0.9,
-  enable_tab_bar = false,
-}
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.font = wezterm.font 'Mononoki Nerd Font'
+config.font_size = 13.0
+config.window_background_opacity = 0.9
+config.hide_tab_bar_if_only_one_tab = true
+
+-- and finally, return the configuration to wezterm
+return config
